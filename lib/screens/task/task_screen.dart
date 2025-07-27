40fb17d058dc843d771ab18c728ac939358cc8e0
Andrushazzz <miraiii99gg@yandex.ru>
2025-07-27 13:53:32 +0700

    fix: fix changes date

--- lib/screens/task/task_screen.dart
+++ lib/screens/task/task_screen.dart
@@ -18,8 +18,6 @@ class TaskScreen extends StatefulWidget {
 class _TaskScreenState extends State<TaskScreen> {
   late PageController _pageController;
   late DateTime _selectedDate;
-  final int _maxDays = 360;
-  bool _isAnimating = false;
 
   @override
   void initState() {
@@ -35,189 +33,187 @@ class _TaskScreenState extends State<TaskScreen> {
   }
 
   void _onDateChanged(DateTime newDate) {
+    final difference = newDate.difference(_selectedDate).inDays;
+    if (difference != 0) {
+      _pageController.animateToPage(
+        _pageController.page!.toInt() + difference,
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.easeInOut,
+      );
+    }
     setState(() {
-      _selectedDate =
-          newDate.isBefore(DateTime.now()) ? DateTime.now() : newDate;
-
-      final daysDifference = _selectedDate.difference(DateTime.now()).inDays;
-      final newPage = daysDifference;
-
-      _isAnimating = true;
-      _pageController.jumpToPage(newPage);
+      _selectedDate = newDate;
     });
   }
 
-  void _onPageChanged(int page) {
-    if (_isAnimating) return;
-
+  void _onPageChanged(int index) {
+    final newDate = DateTime.now().add(Duration(days: index));
     setState(() {
-      if (page < 0) {
-        _selectedDate = DateTime.now();
-        _pageController.jumpToPage(0);
-      } else {
-        _selectedDate = DateTime.now().add(Duration(days: page));
-        _pageController.jumpToPage(page);
-      }
+      _selectedDate = newDate;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TaskBloc, TaskState>(
-      builder: (context, state) {
-        return DecoratedBox(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                context.colors.noirViolet,
-                context.colors.noirVioletBackGradient,
-              ],
-              stops: const [0.7, 1],
-            ),
-          ),
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            body: SafeArea(
-              child: Column(
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            context.colors.noirViolet,
+            context.colors.noirVioletBackGradient,
+          ],
+          stops: const [0.7, 1],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Column(
                 children: [
-                  Column(
-                    children: [
-                      AppPadding(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            RoundButton(
-                              onTap: () => context.push(Routes.settings.path),
-                              icon: context.icons.user,
-                            ),
-                            DateSelector(
-                              selectedDate: _selectedDate,
-                              onDateChanged: _onDateChanged,
-                            ),
-                            RoundButton(
-                              onTap: () => context.push(Routes.settings.path),
-                              icon: context.icons.setting,
-                            ),
-                          ],
+                  AppPadding(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        RoundButton(
+                          onTap: () => context.push(Routes.settings.path),
+                          icon: context.icons.user,
                         ),
-                      ),
-                      const SizedBox(height: 30),
-                      AppPadding(
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              BlocSelector<UserBloc, UserState, String?>(
-                                selector: (state) {
-                                  return state.userName;
-                                },
-                                builder: (context, name) {
-                                  return Text(
-                                    'Hello, $name',
-                                    style: context.theme.textTheme.displayLarge,
-                                  );
-                                },
-                              ),
-                              Text(
-                                'Have a nice day',
-                                style: context.theme.textTheme.titleMedium!
-                                    .copyWith(color: context.colors.gray),
-                              ),
-                            ],
-                          ),
+                        DateSelector(
+                          selectedDate: _selectedDate,
+                          onDateChanged: _onDateChanged,
                         ),
-                      ),
-                      SizedBox(
-                        height: 100,
-                        child: Row(
-                          children: [
-                            const Expanded(child: CategoryTabBar()),
-                            const SizedBox(width: 15),
-                            RoundButton(
-                              onTap: () {},
-                              icon: context.icons.trash,
-                            ),
-                          ],
+                        RoundButton(
+                          onTap: () => context.push(Routes.settings.path),
+                          icon: context.icons.setting,
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                  Expanded(
-                    child: AppPadding(
-                      child: BlocBuilder<TaskBloc, TaskState>(
-                        builder: (context, state) {
-                          return PageView.builder(
-                            controller: _pageController,
-                            onPageChanged: _onPageChanged,
-                            itemCount: _maxDays,
-                            itemBuilder: (context, index) {
-                              final todos =
-                                  state.tasks.where((task) {
-                                    return task.createdAt.year ==
-                                            _selectedDate.year &&
-                                        task.createdAt.month ==
-                                            _selectedDate.month &&
-                                        task.createdAt.day == _selectedDate.day;
-                                  }).toList();
-                              if (todos.isEmpty) {
-                                return const Center(
-                                  child: Text('Нет задач на эту дату'),
-                                );
-                              }
-                              return ListView.separated(
-                                itemCount: todos.length,
-                                itemBuilder: (context, index) {
-                                  return TaskCard(
-                                    title: todos[index].name,
-                                    description: todos[index].description ?? '',
-                                    createdAt: DateFormat(
-                                      'dd.MM.yyyy',
-                                    ).format(todos[index].createdAt),
-                                    marker:
-                                        todos[index].priority != null
-                                            ? TaskMarker(
-                                              text:
-                                                  todos[index].category ?? '?',
-                                              color: todos[index].priority!,
-                                            )
-                                            : null,
-                                    isCompleted: todos[index].isCompleted,
-                                    onCheckboxChanged: (newValue) {
-                                      context.read<TaskBloc>().add(
-                                        ToggleCompleteTaskEvent(
-                                          id: todos[index].id,
-                                          isCompleted: newValue ?? false,
-                                        ),
-                                      );
-                                    },
-                                    onSelected: (value) {
-                                      if (value == 'edit') {}
-                                      if (value == 'delete') {
-                                        context.read<TaskBloc>().add(
-                                          DeleteTaskEvent(id: todos[index].id),
-                                        );
-                                      }
-                                    },
-                                  );
-                                },
-                                separatorBuilder: (context, index) {
-                                  return const SizedBox(height: 15);
-                                },
+                  const SizedBox(height: 30),
+                  AppPadding(
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          BlocSelector<UserBloc, UserState, String?>(
+                            selector: (state) {
+                              return state.userName;
+                            },
+                            builder: (context, name) {
+                              return Text(
+                                'Hello, $name',
+                                style: context.theme.textTheme.displayLarge,
                               );
                             },
-                          );
-                        },
+                          ),
+                          Text(
+                            'Have a nice day',
+                            style: context.theme.textTheme.titleMedium!
+                                .copyWith(color: context.colors.gray),
+                          ),
+                        ],
                       ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 100,
+                    child: Row(
+                      children: [
+                        const Expanded(child: CategoryTabBar()),
+                        const SizedBox(width: 15),
+                        RoundButton(onTap: () {}, icon: context.icons.trash),
+                      ],
                     ),
                   ),
                 ],
               ),
-            ),
+              Expanded(
+                child: PageView.builder(
+                  controller: _pageController,
+                  onPageChanged: _onPageChanged,
+                  itemBuilder: (context, index) {
+                    final date = DateTime.now().add(Duration(days: index));
+                    return AppPadding(child: _TaskListForDate(date: date));
+                  },
+                ),
+              ),
+            ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TaskListForDate extends StatelessWidget {
+  final DateTime date;
+
+  const _TaskListForDate({super.key, required this.date});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TaskBloc, TaskState>(
+      builder: (context, state) {
+        final todos =
+            state.tasks.where((task) {
+              return task.createdAt.year == date.year &&
+                  task.createdAt.month == date.month &&
+                  task.createdAt.day == date.day;
+            }).toList();
+
+        if (todos.isEmpty) {
+          return Center(
+            child: Text(
+              'Тут задач нет',
+              style: context.theme.textTheme.bodyMedium,
+            ),
+          );
+        }
+
+        return ListView.separated(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            return TaskCard(
+              title: todos[index].name,
+              description: todos[index].description ?? '',
+              createdAt: DateFormat(
+                'dd.MM.yyyy',
+              ).format(todos[index].createdAt),
+              marker:
+                  todos[index].priority != null
+                      ? TaskMarker(
+                        text: todos[index].category ?? '?',
+                        color: todos[index].priority!,
+                      )
+                      : null,
+              isCompleted: todos[index].isCompleted,
+              onCheckboxChanged: (newValue) {
+                context.read<TaskBloc>().add(
+                  ToggleCompleteTaskEvent(
+                    id: todos[index].id,
+                    isCompleted: newValue ?? false,
+                  ),
+                );
+              },
+              onSelected: (value) {
+                if (value == 'edit') {}
+                if (value == 'delete') {
+                  context.read<TaskBloc>().add(
+                    DeleteTaskEvent(id: todos[index].id),
+                  );
+                }
+              },
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const SizedBox(height: 15);
+          },
         );
       },
     );
